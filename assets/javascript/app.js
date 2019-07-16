dab7311a00982a5078688651e53a85481be537a6
Rick <rick.carnes@gmail.com>
2019-07-16 09:40:43 -0400

    added portfolio dynamically

---
+++ assets/javascript/app.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    //Setting the "My Projects" button to change the layout and display portfolio by altering CSS, using .hide, and .append
+    $('#projectsBtn').click(function() {
+        $('body').css("background", "url('assets/images/backgrounds/projectBackground.jpg') no-repeat center center fixed");
+        $('body').css("-webkit-background-size", "cover");
+        $('body').css("-moz-background-size", "cover");
+        $('body').css("-o-background-size", "cover");
+        $('body').css("background-size", "cover");
+        $('#name').hide();
+        $('#myTitle').hide();
+        $('#aboutMe').hide();
+        $('#projectsBtn').hide();
+        $('#resume').css("margin-top", "60px");
+        $('#email').css("margin-top", "60px");
+        $('#linkedin').css("margin-top", "60px");
+        $('#github').css("margin-top", "60px");
+        $('#resume').css("margin-right", "260px");
+        $('body').append('<div id="projectRow" class="row"></div>');
+        $('#projectRow').append('<div id="projectBox" class="col-lg-12"></div>');
+        $('#projectBox').append('<button id="backBtn" type="button" class="btn btn-light">Back</button>');
+
+        //Asigning code to the "Back" button to function as expected
+        $('#backBtn').click(function() {
+            location.reload();
+        });
+    });
+
+
+});
