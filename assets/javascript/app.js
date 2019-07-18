772ad09434a7f88cd175b5e89530296bae84581c
Rick <rick.carnes@gmail.com>
2019-07-18 17:18:52 -0400

    fixed funionality of the next and previous buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,36 +2,6 @@ $(document).ready(function() {
     //Hiding the slideshow on the main page
     $('#slideshow').hide();
 
-    //Adding function for the slideshow
-    var slideIndex = 1;
-    showSlides(slideIndex);
-
-    // Next/previous controls
-    function plusSlides(n) {
-        showSlides(slideIndex += n);
-    }
-
-    // Thumbnail image controls
-    function currentSlide(n) {
-        showSlides(slideIndex = n);
-    }
-
-    function showSlides(n) {
-        var i;
-        var slides = document.getElementsByClassName("mySlides");
-        var dots = document.getElementsByClassName("dot");
-        if (n > slides.length) { slideIndex = 1 }
-        if (n < 1) { slideIndex = slides.length }
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" active", "");
-        }
-        slides[slideIndex - 1].style.display = "block";
-        dots[slideIndex - 1].className += " active";
-    }
-
     //Setting the "My Projects" button to change the layout and display portfolio by altering CSS, using .hide, and .append
     $('#projectsBtn').click(function() {
         $('#slideshow').show();
@@ -58,4 +28,55 @@ $(document).ready(function() {
             location.reload();
         });
     });
+
+    //Adding function for the slideshow
+    var slideIndex = 1;
+    showSlides(slideIndex);
+
+    // Next/previous controls
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    $('.next').click(function() {
+        plusSlides(1);
+    });
+
+    $('.prev').click(function() {
+        plusSlides(-1);
+    });
+
+    $('#dot-1').click(function() {
+        currentSlide(1);
+    });
+
+    $('#dot-2').click(function() {
+        currentSlide(2);
+    });
+
+    $('#dot-3').click(function() {
+        currentSlide(3);
+    });
+
+    // Thumbnail image controls
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+        if (n > slides.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = slides.length }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+    }
+
 });
