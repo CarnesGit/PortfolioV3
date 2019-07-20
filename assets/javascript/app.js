5f560d82d0b449341c08ca70f4f46896a083d17a
Rick <rick.carnes@gmail.com>
2019-07-19 23:47:53 -0400

    Cleaned up code and added resume

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,23 +4,33 @@ $(document).ready(function() {
 
     //Setting the "My Projects" button to change the layout and display portfolio by altering CSS, using .hide, and .append
     $('#projectsBtn').click(function() {
+        //Show the slideshow
         $('#slideshow').show();
+
+        //Change the background image
         $('body').css("background", "url('assets/images/backgrounds/projectBackground.jpg') no-repeat center center fixed");
         $('body').css("-webkit-background-size", "cover");
         $('body').css("-moz-background-size", "cover");
         $('body').css("-o-background-size", "cover");
         $('body').css("background-size", "cover");
+
+        //Hiding elements from the home page
         $('#name').hide();
         $('#myTitle').hide();
         $('#aboutMe').hide();
         $('#projectsBtn').hide();
+
+        //Tweaking CSS for the Projects page
         $('#resume').css("margin-top", "60px");
         $('#email').css("margin-top", "60px");
         $('#linkedin').css("margin-top", "60px");
         $('#github').css("margin-top", "60px");
         $('#resume').css("margin-right", "260px");
+
+        //Adding the new container for the projects page
         $('body').append('<div class="container-4"></div>');
-        // $('.container-4').append('<div class="box-6"></div>');
+
+        // Creating the Back (Home) Button
         $('.container-1').prepend('<button id="backBtn" type="button">Home</button>');
 
         //Asigning code to the "Back" button to function as expected
@@ -78,9 +88,5 @@ $(document).ready(function() {
         slides[slideIndex - 1].style.display = "block";
         dots[slideIndex - 1].className += " active";
     }
-
-    $('#hangCode').click(function() {
-        location.reload();
-    });
 
 });
